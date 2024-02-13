36d56607c3168b2679c2c0a33ed360aa7d1c7c23
adam.pentkowski124@gmail.com <adam.pentkowski124@gmail.com>
2024-02-13 18:06:28 +0100

    moved to windows form

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿
 namespace GCity
 {
-	internal class Program
+	public partial class Program : Form 
 	{
 		private static void Main(string[] args)
 		{
 
 			OSMData osmData = Loader.LoadOSMFromGeoJSON("map/roads.geojson");
 			Console.WriteLine("Hello, World!");
+			Application.Run(new Program());
 		}
 	}
 }
